5f66eaf0544976202b20a952618ad7ce38e69626
celinechole <github@celinechole.com>
2019-01-29 12:06:31 -0500

    Tries lastIndexOf spec

--- wrap.spec.js
+++ wrap.spec.js
@@ -1,13 +1,19 @@
 const { expect } = require('chai');
 const wrap = require('./wrap');
 
+const line = 'Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.'
+
 describe('wrap', () => {
   it('Returns empty string if empty string was provided', () => {
     expect(wrap("", 10)).to.equal("");
   });
 
-  it('Lines length is longer than column number', () => {
-      const line = 'Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.'
+  it('Returns line if length is longer than column number', () => {
       expect(wrap(line, 100)).to.equal(line);
   });
+
+  it('Returns index of empty string', () => {
+    // expect(line.lastIndexOf(' ')).to.equal()
+    expect(' ')
+  })
 });
